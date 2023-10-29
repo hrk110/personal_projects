3d4591f5d65f3e8b1e459a45d32c82b4a4ec8145
hrk110 <85362036+hrk110@users.noreply.github.com>
2023-10-29 10:46:56 +0900

    now can avoid abstract length limitation

--- tools_for_Notion/paper_retriever/paper_retriever.py
+++ tools_for_Notion/paper_retriever/paper_retriever.py
@@ -39,7 +39,11 @@ if "abstract" in data["message"]:
   s = BeautifulSoup(data["message"]["abstract"], "html.parser")
   abstract = s.find_all("jats:p")
   abstract_content = [x.text for x in abstract]
-  print("abstract added")
+  abstract_length = sum([len(text) for text in abstract_content])
+  print(f"abstract found, the length is {abstract_length}.")
+  if abstract_length > 2000:
+    logger.warning(f"the length exceed the limit. get whole sentence manually.")
+    abstract_content = []
 else:
   logger.warning("abstract not found! find it by yourself.")
 
@@ -113,5 +117,6 @@ for p in abstract_content:
 notion_response = requests.post(notion_url, headers=notion_headers, json=notion_payload)
 if notion_response.status_code != 200:
   logger.error(f"Error: Unable to post data to {notion_url}. Status code: {notion_response.status_code}")
+  logger.error(f"Response:\n{notion_response.json()}")
   sys.exit(1)
 print('paper_reetriever.py successfully finished!')
